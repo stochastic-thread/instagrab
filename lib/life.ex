61623314152068cdf65eb1d813c367286e266b87
Arthur Colle <hello@arthurcolle.com>
2015-06-18 17:21:30 -0400

    Change to Task

--- lib/life.ex
+++ lib/life.ex
@@ -1,18 +1,18 @@
 defmodule Life do
   def start_link do
-  	vita = spawn(fn -> loop([]) end)
+  	vita = Task.start_link(fn -> loop([]) end)
   	send vita, {:add, "https://api.instagram.com/v1/users/self/media/recent?access_token=1531139.f18ce1e.1353009936304f85921167c58a591475"}
   end
 
-  def loop(list_perm) do
+  def loop(url_list) do
   	receive do
       {:add, url} ->
       	resp = HTTPoison.get! url
       	json_body = Poison.decode! resp.body
       	list = json_body["data"] |> Enum.map fn(x) -> x["images"]["standard_resolution"]["url"] end
       	send self(), {:add, json_body["pagination"]["next_url"]}
-      	IO.puts list_perm
-      	loop(list_perm ++ list)
+      	IO.puts url_list
+      	loop(url_list ++ list)
   	end
   end
 end
